977e3ba3b637df9df2a2fb5c4e0cf62c1596a5d7
주영 <jyk102999@dsm.hs.kr>
2023-07-04 17:45:30 +0900

    ♻️ :: modifyAt 삭제

--- src/main/java/com/example/qpid/global/entity/BaseTimeEntity.java
+++ src/main/java/com/example/qpid/global/entity/BaseTimeEntity.java
@@ -2,7 +2,6 @@ package com.example.qpid.global.entity;
 
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.EntityListeners;
@@ -16,7 +15,4 @@ public class BaseTimeEntity {
 
     @CreatedDate
     private LocalDateTime createdAt;
-
-    @LastModifiedDate
-    private LocalDateTime modifiedAt;
 }
