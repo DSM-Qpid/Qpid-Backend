1880a293e60b108dafb8475abba6421c114178c1
jyk1029 <jyk102999@gmail.com>
2023-07-04 13:48:33 +0900

    ⚙ :: 프로젝트 세팅

---
+++ src/main/java/com/example/qpid/QpidApplication.java
@@ -0,0 +1,13 @@
+package com.example.qpid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QpidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QpidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/qpid/QpidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qpid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QpidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
